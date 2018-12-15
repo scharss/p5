7267f20b13d2da26b47b0fd298b06bfcddad1b03
Scharss <scharss@gmail.com>
2018-12-15 10:14:03 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(600, 400);
+	background(250,250,100);
+}
+
+function draw() {
+  
+	
+	//ellipse
+	noStroke()
+	fill(250,200,200);
+	ellipse(mouseX,mouseY,25,25,);
+	
+	//rectangle
+	//fill(200,250,200);
+	//rect(400,100,50,50);
+}
+
+function mousePressed(){
+ background(250,250,100);
+}
